7a858731af59c5317162f6432365e568806a8ab5
yyy2015 <2456700983@qq.com>
2017-02-16 17:04:00 +0800

    to be solved characterCoding problem

--- h6/StudentGradeJPAEJBWeb/src/edu/nju/student/grade/filter/CharacterCodingFilter.java
+++ h6/StudentGradeJPAEJBWeb/src/edu/nju/student/grade/filter/CharacterCodingFilter.java
@@ -8,7 +8,7 @@ import java.io.IOException;
  * Created by yyy on 2016/12/20.
  */
 
-@WebFilter("/Grade")
+@WebFilter("/*")
 public class CharacterCodingFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -17,6 +17,7 @@ public class CharacterCodingFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.setCharacterEncoding("utf-8");
         response.setContentType("text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
         chain.doFilter(request,response);
         return;
     }
